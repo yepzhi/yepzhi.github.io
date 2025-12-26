21375268001c6bc4bb58d910fcee5b6d4f50d731
Alberto Yepiz <yepzhi@gmail.com>
2025-12-26 03:38:55 -0700

    chore: bump version to v4.1.3 for cache busting

--- translations.js
+++ translations.js
@@ -35,7 +35,7 @@ const translations = {
         footer: {
             developed: "Developed by <a href=\"https://github.com/yepzhi\" target=\"_blank\">@yepzhi</a>",
             visitors: "Visitors 👀",
-            rights: "© 2025 Alberto Yépiz • All Rights Reserved • v4.1.2"
+            rights: "© 2025 Alberto Yépiz • All Rights Reserved • v4.1.3"
         }
     },
     en: {
@@ -74,7 +74,7 @@ const translations = {
         footer: {
             developed: "Developed by <a href=\"https://github.com/yepzhi\" target=\"_blank\">@yepzhi</a>",
             visitors: "Visitors 👀",
-            rights: "© 2025 Alberto Yépiz • All Rights Reserved • v4.1.2"
+            rights: "© 2025 Alberto Yépiz • All Rights Reserved • v4.1.3"
         }
     }
 };
