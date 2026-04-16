0d758e27cf8c64a79295c70b2eb60959797ef7eb
Alberto Yepiz <yepzhi@gmail.com>
2026-04-16 16:00:12 -0700

    chore: update footer copyright year to 2026 and version to v4.2.0

--- translations.js
+++ translations.js
@@ -57,7 +57,7 @@ const translations = {
         footer: {
             developed: "Developed by <a href=\"https://github.com/yepzhi\" target=\"_blank\">@yepzhi</a>",
             visitors: "Visitors 👀",
-            rights: "© 2025 Alberto Yépiz • All Rights Reserved • v4.2.1"
+            rights: "© 2026 Alberto Yépiz • All Rights Reserved • v4.2.0"
         }
     },
     en: {
@@ -118,7 +118,7 @@ const translations = {
         footer: {
             developed: "Developed by <a href=\"https://github.com/yepzhi\" target=\"_blank\">@yepzhi</a>",
             visitors: "Visitors 👀",
-            rights: "© 2025 Alberto Yépiz • All Rights Reserved • v4.2.1"
+            rights: "© 2026 Alberto Yépiz • All Rights Reserved • v4.2.0"
         }
     },
     cn: {
@@ -179,7 +179,7 @@ const translations = {
         footer: {
             developed: "由 <a href=\"https://github.com/yepzhi\" target=\"_blank\">@yepzhi</a> 开发",
             visitors: "访客 👀",
-            rights: "© 2025 Alberto Yépiz • 版权所有 • v4.2.1"
+            rights: "© 2026 Alberto Yépiz • 版权所有 • v4.2.0"
         }
     }
 };
