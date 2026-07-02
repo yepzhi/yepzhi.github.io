70c3618cea035253832517b717d21073f46b5987
Alberto Yepiz <yepzhi@gmail.com>
2026-07-02 10:06:54 -0700

    fix: bump version to v4.2.6 to force cache invalidation for translations.js

--- translations.js
+++ translations.js
@@ -90,7 +90,7 @@ const translations = {
         footer: {
             developed: "Developed by <a href=\"https://github.com/yepzhi\" target=\"_blank\">@yepzhi</a>",
             visitors: "Visitors 👀",
-            rights: "© 2026 Alberto Yépiz • All Rights Reserved • v4.2.5"
+            rights: "© 2026 Alberto Yépiz • All Rights Reserved • v4.2.6"
         }
     },
     en: {
@@ -184,7 +184,7 @@ const translations = {
         footer: {
             developed: "Developed by <a href=\"https://github.com/yepzhi\" target=\"_blank\">@yepzhi</a>",
             visitors: "Visitors 👀",
-            rights: "© 2026 Alberto Yépiz • All Rights Reserved • v4.2.5"
+            rights: "© 2026 Alberto Yépiz • All Rights Reserved • v4.2.6"
         }
     },
     cn: {
@@ -278,7 +278,7 @@ const translations = {
         footer: {
             developed: "由 <a href=\"https://github.com/yepzhi\" target=\"_blank\">@yepzhi</a> 开发",
             visitors: "访客 👀",
-            rights: "© 2026 Alberto Yépiz • 版权所有 • v4.2.5"
+            rights: "© 2026 Alberto Yépiz • 版权所有 • v4.2.6"
         }
     }
 };
